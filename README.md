ed94e5b62c9b33b9381a9200164a73e5612779dc
aparnaroy <aparnar@udel.edu>
2024-09-21 18:46:32 -0400

    formatted checklist

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # Taskly
 
 A To-Do App
-
-## Attributions
-
-- <a href="https://www.flaticon.com/free-icons/sidebar" title="sidebar icons">Sidebar icons created by bqlqn - Flaticon</a>
--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 function toggleNav() {
     const sidebarIcon = document.getElementById('sidebarIcon');
+    const mainContent = document.querySelector('.main-content');
+
     // Toggle between black and white
     if (sidebarIcon.src.includes('sidebar-icon-black.png')) {
         sidebarIcon.src = './img/sidebar-icon-white.png';
@@ -9,6 +11,8 @@ function toggleNav() {
     
     // Toggle sidebar open/close
     $('#side-nav').toggleClass('open');
+    // Toggle left-align class for main content when sidebar is opened/closed
+    mainContent.classList.toggle('left-align');
 }
 
 function toggleLightDarkMode() {
@@ -31,4 +35,23 @@ function toggleLightDarkMode() {
     } else {
         lightDarkIcon.src = './img/lightdark-icon-white.png';
     }
+}
+
+function addTask() {
+    const input = document.getElementById('newTaskInput');
+    const taskList = document.getElementById('taskList');
+
+    if (input.value.trim() === '') return; // Ignore empty input
+
+    const newTask = document.createElement('li');
+    newTask.innerHTML = `<span class="circle" onclick="taskCompleted(event)"></span>${input.value}`;
+    taskList.appendChild(newTask);
+
+    input.value = ''; // Clear the input field
+}
+
+function taskCompleted(event) {
+    // Handle the click on the circle here (e.g., toggle a completed state)
+    const circle = event.currentTarget;
+    circle.classList.toggle('clicked'); // You can style this class to indicate completion
 }
