e29584d0bcfc045590ae94b066a6c732df15ebbb
aparnaroy <aparnar@udel.edu>
2024-09-22 20:33:27 -0400

    celebration is longer and fixed tag add button dark mode

--- js/main.js
+++ js/main.js
@@ -136,7 +136,7 @@ function taskCompleted(event) {
 
 
 function createConfetti() {
-    const numConfetti = 300; // Number of confetti pieces
+    const numConfetti = 2000; // Number of confetti pieces
     const colors = [
         '#FF5733', // Red
         '#FFC300', // Yellow
